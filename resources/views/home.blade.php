b6f3eed87d59d9f3dc63afe241f6a099ab455ed2
BigboiBoyd <162144358+BigboiBoyd@users.noreply.github.com>
2024-04-14 19:13:43 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-0"></div>
+        
+        <img src="/images/logo.png" class="rounded-circle" style="width:170px;height:160px"  alt="">
+    <div class="col-4"><h1></h1>
+    <div class="mx-auto" style="width: 200px;">
+    <h1>Testicles</h1>
+    <div class="d-flex" style="padding-top:10px;gap:150px;width:100px">
+        <div><strong> posts 153</strong></div>
+        <div><strong> followers 23k</strong></div>
+        <div><strong> following 212</strong></div>
+    </div>
+    <div class="pt-3" style="font-weight: 700;">links</div>
+        <div class="pt-0"><a href="https://discord.gg/J6YrAwjC">Discord</a></div>
+        <div class="pt-0"><a href="https://github.com/BigboiBoyd">Github</a></div>
+</div>
+</div>
+</div>
+
+    
+</div>
+@endsection
